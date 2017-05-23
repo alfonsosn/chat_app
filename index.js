30da2ae61effe483434499615d79710e95be5d66
Alfonso Enriquez Castro <alfonso.en.ca@gmail.com>
2017-05-22 23:14:19 -0400

    base minimum for assignments

--- index.js
+++ index.js
@@ -6,8 +6,6 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
-
 io.on('connection', function(socket){
   console.log('a user connected');
 
@@ -16,13 +14,9 @@ io.on('connection', function(socket){
   });
 
   socket.on('chat message', function(msg){
-    console.log('message: ' + msg);
     io.emit('chat message', msg);
   });
-
 });
-
-
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
